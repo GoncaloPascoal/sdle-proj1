c66798e505776e646b8627a3b04c2d0c6de26796
Gonçalo Pascoal <goncalojpascoal@gmail.com>
2021-11-06 13:58:48 +0000

    Add arguments to subparsers

--- src/rpc.py
+++ src/rpc.py
@@ -3,10 +3,10 @@ from argparse import ArgumentParser
 
 def main():
     parser = ArgumentParser(description='Utility program to perform RPC on \
-            Publisher and Subscriber processes')
+            Publisher and Subscriber processes.')
     
     subparsers = parser.add_subparsers(
-        description='Methods offered by the RPC utility',    
+        description='Methods offered by the RPC utility.',    
         help='name of the method to call',
         dest='method',
     )
@@ -15,6 +15,14 @@ def main():
     parser_get   = subparsers.add_parser('GET'  , help='consume one (1) message from a topic')
     parser_sub   = subparsers.add_parser('SUB'  , help='subscribe to a specific topic')
     parser_unsub = subparsers.add_parser('UNSUB', help='unsubscribe to a specific topic')
+
+    parser_put.add_argument('publisher_id', help='id of the publisher')
+    parser_put.add_argument('topic_id', help='id of the topic')
+    parser_put.add_argument('message', help='message to send')
+
+    for subparser in [parser_get, parser_sub, parser_unsub]:
+        subparser.add_argument('subscriber_id', help='id of the subscriber')
+        subparser.add_argument('topic_id', help='id of the topic')
 
     args = parser.parse_args()
 
