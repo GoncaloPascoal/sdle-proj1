4753ee823097338904d499cf03f49a6ed8f558d3
Gonçalo Pascoal <goncalojpascoal@gmail.com>
2021-11-06 22:09:53 +0000

    Add proxy.py and related CLI args

--- src/subscriber.py
+++ src/subscriber.py
@@ -3,7 +3,10 @@ from argparse import ArgumentParser
 
 def main():
     parser = ArgumentParser(description='Process that subscribes to topics and receives messages.')
+    
     parser.add_argument('id', help='id of the subscriber')
+    parser.add_argument('proxy_addr', help='address of the proxy to connect to')
+    parser.add_argument('proxy_port', type=int, help='port of the proxy to connect to')
 
     args = parser.parse_args()
 
