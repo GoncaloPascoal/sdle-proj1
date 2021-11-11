a76494c89251fe8de619c38a3247460e30e66547
Gonçalo Pascoal <goncalojpascoal@gmail.com>
2021-11-11 19:27:18 +0000

    Start implementing proxy using XSUB and XPUB sockets

--- src/proxy.py
+++ src/proxy.py
@@ -1,4 +1,5 @@
 
+import zmq
 from argparse import ArgumentParser
 
 def main():
@@ -6,10 +7,29 @@ def main():
             between publishers and subscribers, so that subscribers and \
             publishers do not need to know about each other.')
 
-    parser.add_argument('port', type=int, help='port where publishers and \
-            subscribers can connect to')
-    
+    parser.add_argument('publisher_port', type=int, help='port where publishers \
+            can connect to')
+    parser.add_argument('subscriber_port', type=int, help='port where subscribers \
+            can connect to')
+
     args = parser.parse_args()
+
+    context = zmq.Context()
+
+    # Socket for publishers
+    frontend = context.socket(zmq.XSUB)
+    frontend.connect(f'tcp://127.0.0.1:{args.publisher_port}')
+    
+    # Socket for subscribers
+    backend = context.socket(zmq.XPUB)
+    backend.bind(f'tcp://127.0.0.1:{args.subscriber_port}')
+
+    # Run the proxy
+    zmq.proxy(frontend, backend)
+
+    frontend.close()
+    backend.close()
+    context.destroy()
 
 if __name__ == '__main__':
     main()
