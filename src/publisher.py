c690dd690ac9adc1a917cd39e89b20c6021bc536
Gonçalo Pascoal <goncalojpascoal@gmail.com>
2021-11-18 22:45:50 +0000

    Implement RPC with REQ/REP sockets

--- src/publisher.py
+++ src/publisher.py
@@ -1,12 +1,16 @@
 
-from time import sleep
 import zmq
 from argparse import ArgumentParser
+
+def put(sock_proxy: zmq.Socket, message: str):
+    print('PUT message: ', message)
+    sock_proxy.send_string(message)
 
 def main():
     parser = ArgumentParser(description='Process that publishes messages to topics.')
     
     parser.add_argument('id', help='id of the publisher')
+    parser.add_argument('port', help='publisher listening port')
     parser.add_argument('proxy_addr', help='address of the proxy to connect to')
     parser.add_argument('proxy_port', type=int, help='port of the proxy to connect to')
 
@@ -15,16 +19,26 @@ def main():
     context = zmq.Context()
 
     # Connect to proxy
-    socket = context.socket(zmq.PUB)
-    socket.connect(f'tcp://{args.proxy_addr}:{args.proxy_port}')
+    sock_proxy = context.socket(zmq.PUB)
+    sock_proxy.connect(f'tcp://{args.proxy_addr}:{args.proxy_port}')
+
+    # Socket for listening to commands
+    sock_rpc = context.socket(zmq.REP)
+    sock_rpc.bind(f'tcp://*:{args.port}')
 
     print(f'Publisher #{args.id} online...')
 
-    num = 1
     while True:
-        socket.send_string(f'test #{num}')
-        num += 1
-        sleep(3)
+        command = sock_rpc.recv_json()
+        
+        if (isinstance(command, dict) 
+            and set(['method', 'message']).issubset(command.keys())
+            and command['method'] == 'PUT'):
+            put(sock_proxy, command['message'])
+            sock_rpc.send_string('OK')
+        else:
+            sock_rpc.send_string('Error')
+
 
 if __name__ == '__main__':
     main()
--- src/subscriber.py
+++ src/subscriber.py
@@ -2,10 +2,36 @@
 import zmq
 from argparse import ArgumentParser
 
+def process_command(sock_proxy: zmq.Socket, command: dict):
+    commands = {
+        'GET': get,
+        'SUB': sub,
+        'UNSUB': unsub,
+    }
+
+    if (isinstance(command, dict)
+        and set(['method', 'topic']).issubset(command.keys())
+        and command['method'] in commands.keys()):
+        commands['method'](sock_proxy, command['topic'])
+        return True
+
+    return False
+
+def get(sock_proxy: zmq.Socket, topic: str):
+    string = sock_proxy.recv_string() # TODO: get string from specific topic
+    print(string)
+
+def sub(sock_proxy: zmq.Socket, topic: str):
+    sock_proxy.setsockopt_string(zmq.SUBSCRIBE, topic)
+
+def unsub(sock_proxy: zmq.Socket, topic: str):
+    sock_proxy.setsockopt_string(zmq.UNSUBSCRIBE, topic)
+
 def main():
     parser = ArgumentParser(description='Process that subscribes to topics and receives messages.')
     
     parser.add_argument('id', help='id of the subscriber')
+    parser.add_argument('port', help='subscriber listening port')
     parser.add_argument('proxy_addr', help='address of the proxy to connect to')
     parser.add_argument('proxy_port', type=int, help='port of the proxy to connect to')
 
@@ -14,16 +40,26 @@ def main():
     context = zmq.Context()
 
     # Connect to proxy
-    socket = context.socket(zmq.SUB)
-    socket.connect(f'tcp://{args.proxy_addr}:{args.proxy_port}')
+    sock_proxy = context.socket(zmq.SUB)
+    sock_proxy.connect(f'tcp://{args.proxy_addr}:{args.proxy_port}')
+    
+    # Socket for listening to commands
+    sock_rpc = context.socket(zmq.REP)
+    sock_rpc.bind(f'tcp://*:{args.port}')
 
     print(f'Subscriber #{args.id} online...')
 
-    socket.setsockopt(zmq.SUBSCRIBE, b'test')
+    sock_proxy.setsockopt(zmq.SUBSCRIBE, b'test')
 
     while True:
-        string = socket.recv_string()
-        print(string)
+        command = sock_rpc.recv_json()
+
+        ok = process_command(sock_proxy, command)
+
+        if ok:
+            sock_rpc.send_string('OK')
+        else:
+            sock_rpc.send_string('Error') 
 
 if __name__ == '__main__':
     main()
